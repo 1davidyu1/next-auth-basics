06f2da3f66a22c3b6f5a334ca136151d8bec6541
1davidyu1 <yudavid@msu.edu>
2024-06-30 12:01:58 -0500

    middleware, auth, and route for auth

---
+++ middleware.ts
@@ -0,0 +1,4 @@
+export { auth as middleware } from "@/auth"
+
+// use this to invoke matchers for only authenticated users public or private from https://clerk.com/docs/references/nextjs/auth-middleware
+
