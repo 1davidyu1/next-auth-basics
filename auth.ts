922cab5e3768e7c97ded582d11d65329a4b7dff9
1davidyu1 <yudavid@msu.edu>
2024-07-01 22:47:30 -0500

    middleware complete

--- auth.ts
+++ auth.ts
@@ -1,5 +1,13 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "@/lib/db"
+import authConfig from "./auth.config"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+export const { 
+    handlers: {GET, POST}, 
+    auth 
+} = NextAuth({
+    adapter: PrismaAdapter(db),
+    session: { strategy: "jwt"},
+    ...authConfig,
 })
--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,44 @@
-export { auth as middleware } from "@/auth"
-
 // use this to invoke matchers for only authenticated users public or private from https://clerk.com/docs/references/nextjs/auth-middleware
+// callbacks are good when doing next auth actions like sign out, sign in, etc
 
+import NextAuth from "next-auth"
+import authConfig from "./auth.config"
+
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    authRoutes,
+    publicRoutes
+} from "@/routes"
+
+const { auth } = NextAuth(authConfig)
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+    if (isApiAuthRoute) {
+        return;
+    }
+
+    if (isAuthRoute) {
+        if (isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+        }
+        return;
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+        return Response.redirect(new URL("/auth/login", nextUrl))
+    }
+
+    return;
+})
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
